2f2f8d38a2129a6ed07706aa9a6a6d4d8d518f26
danielzhao07 <daniel.zhao2007@gmail.com>
2026-01-07 01:40:49 -0500

    Add rotation, resizing, flipping, and cropping functionalities to transformations script

--- transformations.py
+++ transformations.py
@@ -16,4 +16,35 @@ def translate(img, x, y):
 translated = translate(img, 100, 100)
 cv.imshow('Translated', translated)
 
+# Rotation
+
+def rotate(img, angle, rotPoint=None):
+    (height, width) = img.shape[:2]
+
+    if rotPoint is None:
+        rotPoint = (width//2, height//2)
+
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width, height)
+
+    return cv.warpAffine(img, rotMat, dimensions)
+
+rotated = rotate(img, -45)
+cv.imshow('Rotated', rotated)
+
+rotated_rotated = rotate(img, -90)
+cv.imshow('Rotated Rotated', rotated_rotated)
+
+# Resizing
+resized = cv.resize(img, (500, 500), interpolation=cv.INTER_CUBIC)
+cv.imshow('Resized', resized)
+
+# Flipping
+flip = cv.flip(img, -1) # 0 x-axis, 1 - y-axis, 2 - both
+cv.imshow('Flip', flip)
+
+# Cropping
+cropped = img[200:400, 300:400]
+cv.imshow('Cropped', cropped)
+
 cv.waitKey(0)
